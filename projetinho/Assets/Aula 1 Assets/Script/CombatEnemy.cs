8a299d823d7b2e390d840ed42fc34b9c5333985e
BIANCA MAYARA NASCIMENTO DE SOUZA <bianca.mayara@academico.ifrn.edu.br>
2023-10-06 16:22:22 -0300

    fazendo o hit do inimigo

--- projetinho/Assets/Aula 1 Assets/Script/CombatEnemy.cs
+++ projetinho/Assets/Aula 1 Assets/Script/CombatEnemy.cs
@@ -11,6 +11,7 @@ public class CombatEnemy : MonoBehaviour
     public float attackDamage;
     public float movementSpeed;
     public float lookRadius;
+    public float colliderRadius = 2;
 
     [Header("Components")]
     private Animator anim;
@@ -19,6 +20,11 @@ public class CombatEnemy : MonoBehaviour
 
     [Header("Others")] 
     private Transform player;
+
+    private bool walking;
+    private bool attacking;
+    private bool hiting;
+    private bool waitFor;
     
     // Start is called before the first frame update
     void Start()
@@ -35,18 +41,88 @@ public class CombatEnemy : MonoBehaviour
         float distance = Vector3.Distance(player.position, transform.position);
         if (distance <= lookRadius)
         {
-            agent.SetDestination(player.position);
             //o personagem está no raio de ação
+            agent.isStopped = false;
+            if (!attacking)
+            {
+                agent.SetDestination(player.position);
+                anim.SetBool("Walk Forward", true);
+                walking = true;
+            }
             if (distance <= agent.stoppingDistance)
             {
-                Debug.Log("bateu");
+                StartCoroutine("Attack");
+            }
+            else
+            {
+                attacking = false;
             }
         }
         else
         {
             //o personagem não está no raio de ação
+            anim.SetBool("Walk Forward", false);
+            agent.isStopped = true; 
+            walking = false;
+            attacking = false;
         }
         
+    }
+    
+    IEnumerator Attack()
+    {
+        if (!waitFor)
+        {
+            waitFor = true;
+            attacking = true;
+            walking = false;
+            anim.SetBool("Walk Forward", false);
+            anim.SetBool("Claw Attack", true);
+            yield return new WaitForSeconds(1.5f);
+            GetPlayer();
+            //yield return new WaitForSeconds(1f);
+            waitFor = false;
+        }
+    }
+
+    void GetPlayer()
+    {
+        foreach (Collider c in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+                //vai causar dano no player 
+                Debug.Log("atacou");
+            }
+        }
+    }
+
+    public void GetHit(float damage)
+    {
+        totalHealth -= damage;
+        //totalHealth = totalHealth - damage;
+        if (totalHealth > 0)
+        {
+            //esta vivo
+            StopCoroutine("Attack");
+            anim.SetTrigger("Take Damage");
+            hiting = true;
+            
+        }
+        else
+        {
+            //esta morto
+            anim.SetTrigger("Die");
+        }
+    }
+
+    IEnumerator RecoverFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("Walk Forward", false);
+        anim.SetBool("Claw Attack", false);
+        hiting = false;
+        waitFor = false;
     }
 
     private void OnDrawGizmosSelected()
